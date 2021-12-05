9cbbe8c487256fbe22cf6e5635e66990da1d666d
ccjmne <ccjmne@gmail.com>
2021-12-05 01:15:41 +0100

    Day 4!

---
+++ src/day4/day4.js
@@ -0,0 +1,127 @@
+'use strict';
+
+import getInput from "../input.js";
+import { range, style, Styles } from "../utils.js";
+
+const BOARD_SIZE = 5;
+
+const [draws, ...boardsData] = getInput(import.meta.url)
+  .split(/\n/g)
+  .filter(line => line !== '')
+  .map(
+    line => line
+      .trimStart()
+      .split(/,|\s+/)
+      .map(value => parseInt(value))
+  );
+
+class Board {
+  /**
+   * Expects `BOARD_SIZE * BOARD_SIZE` cells.
+   * @param { number[] } cells
+   */
+  constructor(cells) {
+    this.cells = cells.map(value => ({ value, drawn: false }));
+  }
+
+  /**
+   * Returns the score of the board if it is winning, `-1` otherwise.
+   * @param { number } draw 
+   * @returns { number }
+   */
+  accept(draw) {
+    const i = this.cells.findIndex(({ value }) => draw === value);
+    if(i === -1) {
+      return -1;
+    }
+
+    this.cells[i].drawn = true;
+    const [row, col] = [Math.floor(i / BOARD_SIZE), i % BOARD_SIZE];
+    return (
+      range(BOARD_SIZE).every(c => this.cells[row * BOARD_SIZE + c].drawn)
+      || range(BOARD_SIZE).every(r => this.cells[r * BOARD_SIZE + col].drawn)
+    ) ? draw * this.cells
+        .filter(({ drawn }) => !drawn)
+        .reduce((score, { value }) => score + value, 0)
+      : -1;
+  }
+}
+
+/**
+ * @returns { Board[] }
+ */
+function computeBoards() {
+  return boardsData
+    .flat()
+    .reduce(
+      ({ boards, cells }, value) => 
+      cells.push(value) && (cells.length === BOARD_SIZE * BOARD_SIZE)
+        ? { boards: [...boards, new Board(cells)], cells: [] }
+        : { boards, cells },
+      { boards: [], cells: [] }
+    ).boards;
+}
+
+/**
+ * @param { Board } board 
+ */
+function drawBoard(board) {
+  return '\n' + board.cells.reduce(
+    ({ rows, cells }, cell) =>
+      cells.push(cell) && (cells.length === BOARD_SIZE)
+        ? { rows: [...rows, cells], cells: []}
+        : { rows, cells },
+    { rows: [], cells: [] }
+  ).rows.map(
+    row => row.map(
+      ({ value, drawn }) => style(
+        String(value).padStart(2), drawn ? Styles.BRIGHT | Styles.UNDERSCORE : Styles.RESET
+      )
+    ).join(' ')
+  ).join('\n')
+}
+
+// PART I:
+
+function first() {
+  const boards = computeBoards();
+  for (const draw of draws) {
+    for (const board of boards) {
+      const score = board.accept(draw);
+      if (-1 !== score) {
+        return { board, draw, score };
+      }
+    }
+  }
+}
+
+console.log(
+  'Part I:',
+  '\n> Final score:', first().score,
+  '\n> Winning draw:', first().draw,
+  '\n> Winning board:', drawBoard(first().board),
+);
+
+// PART II:
+
+function last() {
+  const boards = new Set(computeBoards());
+  for (const draw of draws) {
+    for (const board of boards) {
+      const score = board.accept(draw);
+      if (-1 !== score) {
+        boards.delete(board);
+        if (boards.size === 0) {
+          return { board, draw, score };
+        }
+      }
+    }
+  }
+}
+
+console.log(
+  '\nPart II:',
+  '\n> Last score:', last().score,
+  '\n> Last draw:', last().draw,
+  '\n> Last board:', drawBoard(last().board),
+);
--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,21 @@
 export function range(length) {
   return Array.from({ length }, (_, i) => i);
 }
+
+export const Styles = Object.freeze({ RESET: 0b0000001, BRIGHT: 0b0000010, DIM: 0b0000100, UNDERSCORE: 0b0001000, BLINK: 0b0010000, REVERSE: 0b0100000, HIDDEN: 0b1000000 });
+const STYLES_MAP = { [Styles.RESET]: '\x1b[0m', [Styles.BRIGHT]: '\x1b[1m', [Styles.DIM]: '\x1b[2m', [Styles.UNDERSCORE]: '\x1b[4m', [Styles.BLINK]: '\x1b[5m', [Styles.REVERSE]: '\x1b[7m', [Styles.HIDDEN]: '\x1b[8m' };
+
+/**
+ * @param { string } text 
+ * @param { number } styles A combination of flags from `Styles`.  
+ * For example: `Styles.BRIGHT & Styles.UNDERSCORE`
+ * @returns { string }
+ */
+export function style(text, styles) {
+  return Object.entries(STYLES_MAP)
+    .filter(([flag]) => !!(styles & flag))
+    .map(([, esc]) => esc)
+    .join('')
+    + text
+    + STYLES_MAP[Styles.RESET];
+}
