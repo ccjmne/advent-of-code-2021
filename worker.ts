15b12bf6fbff9753288a3083d97f97bb95e8ff48
Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
2023-12-03 19:21:39 +0100

    Handle computing solutions as Observables
    For now, it merely attempts to capture the last value emitted when the
    Observable completes.

--- worker.ts
+++ worker.ts
@@ -1,3 +1,4 @@
+import { isObservable, lastValueFrom } from 'rxjs'
 import { expose } from 'threads-esm'
 
 import { type SolutionModule } from 'src/solution-module'
@@ -9,5 +10,6 @@ void expose({
   process(year: number, day: number, part: 'partI' | 'partII', input: string): Promise<unknown> {
     return (import(`./src/${year}/${day}/${day}.ts#${Math.random()}`) as Promise<ESModule<SolutionModule<unknown>>>)
       .then(({ default: { parse, [part]: run } }) => run(parse(input)))
+      .then(res => (isObservable(res) ? lastValueFrom(res) : res))
   },
 } as WorkerModule)
